5216549a8610824fa308d44b353d0ce3642a3acb
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-22 10:50:32 -0500

    Update and rename places_brand_freshness.py to places_brand_opening_closing.py

---
+++ places_brand_opening_closing.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from read_data import read_from_gsheets
+import altair as alt
+from datetime import datetime, timedelta
+import pandas as pd
+import streamlit.components.v1 as components
+
+
+
+st.set_page_config(
+    page_title="Places Summary Statistics - Brands Freshness",
+    layout="wide"
+)
+
+#### Brand Open Close #### 
+
+
+brands_open_close_df = read_from_gsheets("Global Places")
+brands_open_close_df = brands_open_close_df[["Country", "Release month", "Brands with at least 1 new opened POI", "Brands with at least 1 new closed POI"]]
+
+brands_open_close_df["Brands with at least 1 new opened POI"] = brands_open_close_df["Brands with at least 1 new opened POI"].str.replace(',', '').astype(int)
+brands_open_close_df["Brands with at least 1 new closed POI"] = brands_open_close_df["Brands with at least 1 new closed POI"].str.replace(',', '').astype(int)
+
+
+for i, value in enumerate(brands_open_close_df['Release month']):
+    try:
+        brands_open_close_df.loc[i, 'Release month'] = pd.to_datetime(value, format='%b %Y').strftime('%Y-%m')
+    except ValueError:
+        brands_open_close_df.loc[i, 'Release month'] = pd.to_datetime(value, format='%B %Y').strftime('%Y-%m')
+
+brands_open_close_df = brands_open_close_df[
+    (pd.to_datetime(brands_open_close_df["Release month"]) >= pd.to_datetime("2021-01")) &
+    (brands_open_close_df["Country"] == "Grand Total")
+]
+
+# st.dataframe(brands_open_close_df)
+
+chart = alt.Chart(brands_open_close_df).mark_circle(size=40).encode(
+    x=alt.X('Release month', title='Release month'),
+    y=alt.Y('Brands with at least 1 new opened POI', title='Brands with at least 1 opening or closing of a POI'),
+    color=alt.value('blue'),
+    tooltip=[alt.Tooltip('Release month'),
+             alt.Tooltip('Brands with at least 1 new opened POI', title='>1 Opened POI', format=',')
+    ]
+) + alt.Chart(brands_open_close_df).mark_circle(size=40).encode(
+    x=alt.X('Release month'),
+    y=alt.Y('Brands with at least 1 new closed POI'),
+    color=alt.value('red'),
+    tooltip=[alt.Tooltip('Release month'),
+             alt.Tooltip('Brands with at least 1 new closed POI', title='>1 Closed POI', format=',')]
+)
+
+chart = chart.properties(
+    width=900,
+    height=500,
+    title=alt.TitleParams(
+        text='Brands with at Least 1 Opening or Closing of a POI',
+        fontSize=18
+    )
+)
+
+st.altair_chart(chart)
+
