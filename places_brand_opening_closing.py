8ec16c0b93851b3e82380fe29e001ab3884407ac
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-23 12:01:28 -0500

    remove html elements

--- places_brand_opening_closing.py
+++ places_brand_opening_closing.py
@@ -61,3 +61,18 @@ chart = chart.properties(
 
 st.altair_chart(chart, use_container_width=True)
 
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+hide_decoration_bar_style = '''
+    <style>
+        header {visibility: hidden;}
+    </style>
+'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
