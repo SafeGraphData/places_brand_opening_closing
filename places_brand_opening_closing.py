7a3b396fcfebb36a85aa09cdc549cd654a5ce160
sgclarissa <148800809+safegraph-clarissa@users.noreply.github.com>
2026-08-22 14:32:13 -0500

    Keep-alive commit - 2026-08-22 19:32:13.269964

--- places_brand_opening_closing.py
+++ places_brand_opening_closing.py
@@ -1205,3 +1205,4 @@ st.markdown(css, unsafe_allow_html=True)
 # Keep-alive comment: 2026-08-21 13:32:45.601426
 # Keep-alive comment: 2026-08-21 23:32:14.275857
 # Keep-alive comment: 2026-08-22 09:32:44.790950
+# Keep-alive comment: 2026-08-22 19:32:12.075688
